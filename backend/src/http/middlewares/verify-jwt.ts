9d36105eb3b8a1d98f881b7bc2b8e6f594c5707f
Kauê <kauevecchia@hotmail.com>
2025-05-31 23:53:46 -0300

    feat: implement jwt verification middleware

---
+++ backend/src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,39 @@
+// src/http/middlewares/verify-jwt.ts
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+import { env } from '../../env'
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string
+      }
+    }
+  }
+}
+
+export async function verifyJWT(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  // Obter o token do cabeçalho Authorization
+  const authHeader = request.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return response.status(401).json({ message: 'Token not provided.' })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decodedToken = verify(token, env.JWT_SECRET)
+
+    request.user = { id: decodedToken.sub as string }
+
+    next()
+  } catch (err) {
+    return response.status(401).json({ message: 'Invalid or expired token.' })
+  }
+}
