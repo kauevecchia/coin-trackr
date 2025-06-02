4e7e743ca449e235e6ce8a0ab5e6ec43376e22dd
Kauê <kauevecchia@hotmail.com>
2025-06-01 23:52:57 -0300

    feat: implement token refresh endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,12 @@
 import express from 'express'
 import { PrismaClient } from './generated/prisma'
+import cookieParser from 'cookie-parser'
+import { userRoutes } from './http/controllers/users/routes'
 
 export const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(userRoutes)
 
 const prisma = new PrismaClient()
--- backend/src/http/controllers/users/routes.ts
+++ backend/src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 const userRoutes = Router()
 
 userRoutes.post('/users', register)
 userRoutes.post('/sessions', authenticate)
+
+userRoutes.post('/token/refresh', refresh)
 
 userRoutes.get('/me', verifyJWT, profile)
 
