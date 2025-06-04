0b8f0db77e8029b5ece3a15d19d2aac553b4ee60
Kauê <kauevecchia@hotmail.com>
2025-06-03 22:38:59 -0300

    feat: create error-handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,14 @@ import express from 'express'
 import { PrismaClient } from './generated/prisma'
 import cookieParser from 'cookie-parser'
 import { userRoutes } from './http/controllers/users/routes'
+import { errorHandler } from './http/middlewares/error-handler'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(userRoutes)
+
+app.use(errorHandler)
 
 const prisma = new PrismaClient()
