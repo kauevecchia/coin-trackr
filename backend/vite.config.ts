2954e776ce64c0f7b4840beb5004b5266a97006b
Kauê <kauevecchia@hotmail.com>
2025-06-09 23:38:51 -0300

    chore: update vite.config.ts

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -4,6 +4,34 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
-    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    environmentMatchGlobs: [['src/http/controllers/**', 'node']],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/prisma/**',
+      '**/src/generated/**',
+    ],
+
+    deps: {
+      external: [
+        /@prisma\/client/,
+        /prisma-client/,
+        /dotenv/,
+        'express',
+        'jsonwebtoken',
+        'bcryptjs',
+        'zod',
+        'cookie-parser',
+        'axios',
+      ],
+      inline: ['tsx'],
+    },
+
+    environment: 'node',
+    globals: true,
+    setupFiles: [],
+    clearMocks: true,
+    mockReset: true,
   },
 })
