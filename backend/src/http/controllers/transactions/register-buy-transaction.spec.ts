d03941b3e44cae4c8150a7ec8331ff34ce41a2f2
Kauê <kauevecchia@hotmail.com>
2025-06-16 23:04:18 -0300

    feat: create register-buy-transaction use-case unit test

---
+++ backend/src/http/controllers/transactions/register-buy-transaction.spec.ts
@@ -0,0 +1,43 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { Server } from 'http'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+let testServer: Server
+
+describe('Register Buy Transaction (e2e)', () => {
+  beforeAll(async () => {
+    testServer = app.listen(0)
+  })
+
+  afterAll(async () => {
+    await new Promise((resolve, reject) => {
+      if (testServer) {
+        testServer.close((err) => {
+          if (err) reject(err)
+          resolve(true)
+        })
+      } else {
+        resolve(true)
+      }
+    })
+  })
+
+  it('should be able to register a buy transaction', async () => {
+    const { token } = await createAndAuthenticateUser(testServer)
+
+    const response = await request(testServer)
+      .post('/transactions/register-buy-transaction')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        userId: 'user-01',
+        cryptoSymbol: 'BTC',
+        quantity: 1,
+        unitPriceAtTransaction: 10000,
+        transactionDate: new Date(),
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
