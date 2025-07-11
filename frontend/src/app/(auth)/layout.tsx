5ce991ed3b53106d4a7f7d679062500ba7c38875
Kauê <kauevecchia@hotmail.com>
2025-07-10 22:58:47 -0300

    style: style auth layout for centering content

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      {children}
+    </div>
+  );
 }
