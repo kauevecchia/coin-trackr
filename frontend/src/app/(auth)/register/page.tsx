f1b9342c60477ffc2b6db6202e64df035aaf7a9a
Kauê <kauevecchia@hotmail.com>
2025-07-12 18:15:33 -0300

    feat: integrate react-hook-form and enhance form validation UI

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,55 +1,120 @@
+"use client";
+
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { RegisterFormInputs, registerFormSchema } from "@/schemas/authSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 
 export default function Register() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<RegisterFormInputs>({
+    resolver: zodResolver(registerFormSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data: RegisterFormInputs) => {
+    console.log(data);
+    reset();
+  };
+
+
+
+    return (
       <Card className="w-full max-w-sm">
         <CardHeader className="flex flex-col items-center justify-center">
-          <CardTitle className="text-2xl font-bold">Welcome to Coin Trackr</CardTitle>
+          <CardTitle className="text-2xl font-bold">
+            Welcome to Coin Trackr
+          </CardTitle>
           <CardDescription className="text-sm text-muted-foreground">
             Create an account to get started
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
+              <div className="grid gap-1">
                 <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+                <Controller
+                  control={control}
+                  name="email"
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  )}
                 />
+                {errors.email && (
+                  <p className="text-red-500 text-sm">{errors.email.message}</p>
+                )}
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                 </div>
-                <Input id="password" type="password" required />
+                <Controller
+                  control={control}
+                  name="password"
+                  render={({ field }) => (
+                    <Input {...field} type="password" required />
+                  )}
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
               </div>
               <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="confirmPassword">Confirm Password</Label>
-                </div>
-                <Input id="confirmPassword" type="password" required />
+                <Label htmlFor="confirmPassword">Confirm Password</Label>
+                <Controller
+                  control={control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <Input {...field} type="password" required />
+                  )}
+                />
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-sm">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
               </div>
+            </div>
+            <div className="mt-6">
+              <Button 
+                type="submit" 
+                className="w-full cursor-pointer"
+              >
+                Register
+              </Button>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Register
-          </Button>
-          <Button variant="link" asChild className="font-normal text-muted-foreground hover:text-primary transition-colors duration-300">
+          <Button
+            variant="link"
+            asChild
+            className="font-normal text-muted-foreground hover:text-primary transition-colors duration-300"
+          >
             <Link href="/login">Already have an account? Login</Link>
           </Button>
         </CardFooter>
       </Card>
-    </div>
-  );
+    );
 }
