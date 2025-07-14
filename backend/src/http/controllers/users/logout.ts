7f292f5484043e6e1ba5f06e51df520701e2b046
Kauê <kauevecchia@hotmail.com>
2025-07-14 01:31:07 -0300

    fix: improve logout funcitonality with proper cookie clearing

--- backend/src/http/controllers/users/logout.ts
+++ backend/src/http/controllers/users/logout.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 
 export async function logout(request: Request, response: Response) {
-  response.clearCookie("refreshToken");
+  response.clearCookie("refreshToken", {
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
 
   return response.status(204).send();
 }
