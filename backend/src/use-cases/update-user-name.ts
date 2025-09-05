e1645b21982e6d9c0801b27c0eaec1c02ca55b00
Kauê <kauevecchia@hotmail.com>
2025-09-04 23:50:09 -0300

    feat: implement UpdateUserNameUseCase

---
+++ backend/src/use-cases/update-user-name.ts
@@ -0,0 +1,28 @@
+import { User } from "@/generated/prisma"
+import { prisma } from "@/lib/prisma"
+import { UsersRepository } from "@/repositories/users-repository"
+
+interface UpdateUserNameUseCaseRequest {
+  userId: string
+  name: string
+}
+
+interface UpdateUserNameUseCaseResponse {
+  user: User
+}
+
+export class UpdateUserNameUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ userId, name }: UpdateUserNameUseCaseRequest): Promise<UpdateUserNameUseCaseResponse> {
+    const user = await this.usersRepository.updateUserName(userId, name)
+
+    if (!user) {
+      throw new Error("User not found")
+    }
+
+    return { user }
+  }
+}
+
+export default UpdateUserNameUseCase
