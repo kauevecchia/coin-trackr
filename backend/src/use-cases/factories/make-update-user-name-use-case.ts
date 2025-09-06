b57a28eeb2242f01350b3d5ccc6cb28c1a790047
Kauê <kauevecchia@hotmail.com>
2025-09-06 01:43:22 -0300

    refactor: update import statements and export function in makeUpdateUserNameUseCase

--- backend/src/use-cases/factories/make-update-user-name-use-case.ts
+++ backend/src/use-cases/factories/make-update-user-name-use-case.ts
@@ -1,11 +1,9 @@
-import { UpdateUserNameUseCase } from "@/use-cases/update-user-name"
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UpdateUserNameUseCase } from '../update-user-name'
 
-const makeUpdateUserNameUseCase = () => {
+export function makeUpdateUserNameUseCase() {
   const usersRepository = new PrismaUsersRepository()
   const updateUserNameUseCase = new UpdateUserNameUseCase(usersRepository)
 
   return updateUserNameUseCase
 }
-
-export default makeUpdateUserNameUseCase
