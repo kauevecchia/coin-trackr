b72ece9187eed2da666ba11e7d2bd40abf1267d5
Kauê <kauevecchia@hotmail.com>
2025-09-07 01:40:00 -0300

    feat: add user account management routes for updating name, resetting password, and deleting account

--- backend/src/http/controllers/users/routes.ts
+++ backend/src/http/controllers/users/routes.ts
@@ -5,6 +5,9 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 import { profile } from './profile'
 import { refresh } from './refresh'
 import { logout } from './logout'
+import { updateUserName } from './update-user-name'
+import { resetPassword } from './reset-password'
+import { deleteAccount } from './delete-account'
 
 const userRoutes = Router()
 
@@ -15,5 +18,8 @@ userRoutes.post('/token/refresh', refresh)
 userRoutes.post('/logout', logout)
 
 userRoutes.get('/me', verifyJWT, profile)
+userRoutes.patch('/users/update-name', verifyJWT, updateUserName)
+userRoutes.patch('/users/reset-password', verifyJWT, resetPassword)
+userRoutes.delete('/users/delete-account', verifyJWT, deleteAccount)
 
 export { userRoutes }
