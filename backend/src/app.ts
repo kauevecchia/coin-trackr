90b49c4bb144394474bac837e800a7ea95aeddc3
Kauê <kauevecchia@hotmail.com>
2025-09-23 23:09:55 -0300

    feat: add admin routes to main application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import { userRoutes } from './http/controllers/users/routes'
 import { cryptoRoutes } from './http/controllers/crypto/routes'
 import { transactionsRoutes } from './http/controllers/transactions/routes'
+import { adminRoutes } from './http/controllers/admin/routes'
 import { errorHandler } from './http/middlewares/error-handler'
 import cors from 'cors'
 
@@ -18,6 +19,7 @@ app.use(
 
 app.use(express.json())
 app.use(cookieParser())
+app.use('/admin', adminRoutes)
 app.use(userRoutes)
 app.use('/cryptos', cryptoRoutes)
 app.use('/transactions', transactionsRoutes)
